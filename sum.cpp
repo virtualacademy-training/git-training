ff064ead90da18c015511068b8c0b9533866940a
Virtual Academy Admin <virtualacademycontact@gmail.com>
2021-08-14 01:46:53 +0300

    Sum functionality enhanced to 3 input

--- sum.cpp
+++ sum.cpp
@@ -6,12 +6,15 @@ int main()
 {
     int a;
     int b;
+    int c;
     int ans;
     cout << "Enter First number: ";
     cin >> a;
     cout << "Enter Second number: ";
     cin >> b;
-    ans = a + b;
+    cout << "Enter Second number: ";
+    cin >> c;
+    ans = a + b + c;
     cout << "Sum is : " << ans;
     return 0;
 }
